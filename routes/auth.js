eaaab4bb2a759f8c7eb7b14e77ee8408dcbccd59
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-10-19 13:01:05 -0500

    Endpoints de remover, crear y login

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,17 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
 
-    res.json({
-        ok: true
-    })
-});
+
+//Importacion de los controladores
+
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
+
+
+router.post('/new', crearUsuario);
+
+router.post('/', loginUsuario );
+
+router.get('/renew', revalidarToken );
 
 module.exports = router;
