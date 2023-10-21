32257dbafa91b922677dbda786a5bdc8c45ecce9
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-10-20 21:04:56 -0500

    express validator con los endpoints de crear y logear usuario implementado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 
 
@@ -6,14 +7,20 @@ const crearUsuario= (req, res = response ) => {
 
     const{ name, email, password } = ( req.body );
 
-    if (name.length < 5) {
+    //Manejo de errores con el express validator:
+    const errors = validationResult ( req );
+
+    if ( !errors.isEmpty() ){
         return res.status(400).json({
-            ok: false,
-            msg: 'El nombre debe ser mayor a 5 letras'
+            ok:false,
+            errors: errors.mapped()
         });
     }
-    
-    res.json({
+
+
+
+    //Mensaje http despues de haber creado un usuario    
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name, email, password
@@ -22,6 +29,15 @@ const crearUsuario= (req, res = response ) => {
 
 const loginUsuario = (req, res = reponse) => {
     
+    //Manejo de errores con el express validator:
+    const errors = validationResult ( req );
+    
+    if ( !errors.isEmpty() ){
+        return res.status(400).json({
+            ok:false,
+            errors: errors.mapped()
+        });
+    }
     
     const{email, password } = ( req.body );
     
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 
@@ -8,9 +9,22 @@ const router = Router();
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
 
 
-router.post('/new', crearUsuario);
+router.post('/new',
+[ //Midlewares de la aplicacion al momento de crear un usuario
+check('name', 'El nombre es obligatorio').not().isEmpty(),
+check('email', 'El email es obligatorio').isEmail(),
+check('password', 'El password debe contener por lo menos 6 caracteres').isLength({min: 6})
 
-router.post('/', loginUsuario );
+], 
+crearUsuario);
+
+
+router.post('/',
+[ //Middlewares al momento de iniciar sesion
+check('email', 'El email es obligatorio').isEmail(), 
+check('password', 'El password debe contener por lo menos 6 caracteres').isLength({min: 6})
+], 
+loginUsuario );
 
 router.get('/renew', revalidarToken );
 
